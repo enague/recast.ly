cee6694628b9d059357558dcc28986461b4117c8
enague <ennague@gmail.com>
2018-05-09 17:31:18 -0700

    progress

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,23 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  // console.log(props.collection[0]);
+  var videos = props.collection[0];
+  // console.log(videos);
+  // videos.forEach(function(video) {
+    var title = videos[1].snippet.title;
+    var description = videos[1].snippet.description;
+  // });
 
+  return (<div className="video-list">
+     <VideoListEntry title={[title]} description={[description]}/>
+  </div>);
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // TODO: Render the `App` component to the DOM
 
+ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<VideoList />, document.getElementById('col-md-5'));
